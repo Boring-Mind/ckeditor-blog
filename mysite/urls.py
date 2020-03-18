edc4276858087bc38e242b1fef241224fc68f174
BoringMind <super.d3280@ya.ru>
2020-03-18 17:30:38 +0300

    fixed url routing

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', include('blog.urls')),
 ]
